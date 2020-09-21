1efef279676b8a6c1e50461003b932c9d5d24f58
shean su <shean012s@gmail.com>
2020-09-21 14:09:49 +0800

    update image components logic

--- src/components/imageDef/index.jsx
+++ src/components/imageDef/index.jsx
@@ -1,13 +1,18 @@
 import React from "react"
 
 const Images = (props) => {
-  let url = props.src
+  let url = props.src || ""
   let idx = url.indexOf("images")
-  let fileName = url.substr(idx + 7)
-  const imageUrl = process.env.NODE_ENV === "production" ? imageDefUrl : ""
-  const src = imageUrl
-    ? `${imageUrl + fileName}`
-    : require(`../../images/${fileName}`)
+  let src = ""
+  if (idx === -1) src = url
+  else {
+    let fileName = url.substr(idx + 7)
+    let imageUrl = process.env.NODE_ENV === "production" ? imageDefUrl : ""
+    src = imageUrl
+      ? `${imageUrl + fileName}`
+      : require(`../../images/${fileName}`)
+  }
+
   return (
     <>
       <img style={{ width: "100%", display: "block" }} src={src} />
